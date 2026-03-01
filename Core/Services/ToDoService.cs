37404b7e89fbd9902947c0395d1f7d90d94ecc52
Dmitriy Tatarkov <d.tatarkov@gmail.com>
2026-03-01 18:38:40 +0300

    Simplified ToDoService.CompleteToDoAsync operation

--- Core/Services/ToDoService.cs
+++ Core/Services/ToDoService.cs
@@ -48,11 +48,6 @@ public class ToDoService(IToDoOwnerFactory toDoOwnerFactory) : IToDoService
 
     public async Task CompleteToDoAsync(Guid todoId)
     {
-        if (todoId == Guid.Empty)
-        {
-            throw new ArgumentException("Invalid todo ID.", nameof(todoId));
-        }
-
         var todoOwner = toDoOwnerFactory.Create();
         var todo = await todoOwner.GetToDoByIdAsync(todoId);
 
