123aa303403d5cb54ed0cb92abd8ce9c8da4ca35
Dmitriy Tatarkov <d.tatarkov@gmail.com>
2026-03-05 12:15:52 +0300

    Fixed Nuxt file path aliases

--- Client.Nuxt/nuxt.config.ts
+++ Client.Nuxt/nuxt.config.ts
@@ -2,5 +2,9 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/storybook']
+  modules: ['@nuxtjs/storybook'],
+
+  alias: {
+    "@/*": "../*",
+  },
 })
