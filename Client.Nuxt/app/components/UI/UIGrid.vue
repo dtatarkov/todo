1bcb7e6b0660b703813c6a522b753c4faf65dd30
Dmitriy Tatarkov <d.tatarkov@gmail.com>
2026-03-13 16:39:21 +0300

    Added UIGrid

---
+++ Client.Nuxt/app/components/UI/UIGrid.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="ui-grid">
+    <slot />
+  </div>
+</template>
+
+<style lang="scss">
+.ui-grid {
+  display: grid;
+  grid-template-columns: repeat(3, minmax(300px, 1fr));
+  gap: 4px;
+}
+</style>
--- Client.Nuxt/app/components/Widgets/ToDosWidget.vue
+++ Client.Nuxt/app/components/Widgets/ToDosWidget.vue
@@ -6,5 +6,15 @@ const data = useViewModel(viewmodel);
 </script>
 
 <template>
-  <ToDoCard v-for="todo of data.todos" :key="todo.id" :todo="todo" />
+  <div class="todos-widget">
+  <UIGrid>
+    <ToDoCard v-for="todo of data.todos" :key="todo.id" :todo="todo" />
+  </UIGrid>
+  </div>
 </template>
+
+<style lang="scss">
+.todos-widget {
+  padding: 16px;
+}
+</style>
