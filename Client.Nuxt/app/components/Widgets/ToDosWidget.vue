847b7229b5574dd5c8a9c37035a07eae7bddfa4a
Dmitriy Tatarkov <d.tatarkov@gmail.com>
2026-03-13 12:41:00 +0300

    Added ToDosWidget

---
+++ Client.Nuxt/app/components/Widgets/ToDosWidget.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { ToDo } from "#shared/entities/todo";
+
+const todo = new ToDo({
+  title: 'Задача 1',
+  description: 'Описание задачи',
+  completionDatePlanned: new Date('2023-12-01'),
+  completionDateActual: undefined
+});
+</script>
+
+<template>
+  <ToDoCard :todo="todo"/>
+</template>
--- Client.Nuxt/app/pages/index.vue
+++ Client.Nuxt/app/pages/index.vue
@@ -1,14 +1,7 @@
 <script setup lang="ts">
-import { ToDo } from "#shared/entities/todo";
-
-const todo = new ToDo({
-  title: 'Задача 1',
-  description: 'Описание задачи',
-  completionDatePlanned: new Date('2023-12-01'),
-  completionDateActual: undefined
-});
+import ToDosWidget from "~/components/Widgets/ToDosWidget.vue";
 </script>
 
 <template>
-  <ToDoCard :todo="todo"/>
+  <ToDosWidget />
 </template>
