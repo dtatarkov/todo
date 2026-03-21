09a2428d12b92fd0cf0aa8673e43377457f7c5a7
Dmitriy Tatarkov <d.tatarkov@gmail.com>
2026-03-21 10:45:09 +0300

    inputElement interface update

--- Client.Nuxt/app/interfaces/inputElement.ts
+++ Client.Nuxt/app/interfaces/inputElement.ts
@@ -3,6 +3,7 @@ import type { InputElementData } from "~/types/inputElementData";
 
 export abstract class InputElement<V = any, D extends InputElementData<V> = InputElementData<V>> extends ViewElement {
   abstract name: string;
+  abstract value: V;
   
   abstract setValue(value: V): void
   abstract setData(data: D): void;
