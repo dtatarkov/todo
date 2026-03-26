e84f7be87c18d0005c1a0c06c6c4d18efc3c544c
Dmitriy Tatarkov <d.tatarkov@gmail.com>
2026-03-26 16:08:27 +0300

    Fixed useViewAsyncData

--- Client.Nuxt/app/composables/useViewAsyncData.ts
+++ Client.Nuxt/app/composables/useViewAsyncData.ts
@@ -3,7 +3,7 @@ import type { Action } from "~/types/action";
 export function useViewAsyncData<T>(handler: () => Promise<T>): { data: Ref<T | undefined>, update: Action }
 export function useViewAsyncData<T>(handler: () => Promise<T>, defaultData: T): { data: Ref<T>, update: Action }
 export function useViewAsyncData<T>(loader: () => Promise<T>, defaultData?: T) {
-  const data = ref(defaultData);
+  const data = shallowRef(defaultData);
   
   async function update() {
     data.value = await loader();
