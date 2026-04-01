36030994f9b30f034ea1ba9e67f149a68539869f
Dmitriy Tatarkov <d.tatarkov@gmail.com>
2026-04-01 16:57:32 +0300

    moved utils to shared module

--- Client.Nuxt/modules/todo/runtime/app/mappers/todoDtoMapperImpl.ts
+++ Client.Nuxt/modules/todo/runtime/app/mappers/todoDtoMapperImpl.ts
@@ -2,7 +2,6 @@ import { ToDoDtoMapper } from "../interfaces/todoDtoMapper";
 import type { ToDoGetDto } from "../types/toDoGetDto";
 import { ToDo } from "../interfaces/todo";
 import { ToDoBase } from "../entities/todoBase";
-import { updatePropertiesWithData } from "~/utils/updatePropertiesWithData";
 
 export class ToDoDtoMapperImpl extends ToDoDtoMapper
 {
